41a9413a693bc73590ff5b113b2f04401385ae1a
Brandon Harding <101778503+bharding216@users.noreply.github.com>
2023-07-26 17:18:14 -0500

    Update to access env variables

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,12 +2,11 @@
 	import { Route, Router } from 'svelte-routing';
 	import About from './About.svelte';
 	import Layout from './Layout.svelte';
-	// export let api_url;
+	export let api_url;
 
+	// console.log(process.env.my_test)
 	let enteredName = ''; // The name entered by the user.
 	let serverResponse = ''; // The response received from the server.
-	// const apiURL = process.env.API_URL;
-	// console.log("API URL:", apiURL);
 
 	async function getName() {
 		let usersName = enteredName.trim();
@@ -15,7 +14,7 @@
 			return;
 		}
 
-		let response = await fetch('http://localhost:2000/say-hi', {
+		let response = await fetch(api_url + '/say-hi', {
 			method: 'POST',
 			headers: {
 			'Content-Type': 'application/json',
@@ -36,10 +35,16 @@
 <Router>
 	<Layout>
 		<Route path="/" let:params>
+			{#if serverResponse}
 			<h1>Received Message: {serverResponse}!</h1> 
-
-			<input type="text" placeholder="Enter your name" bind:value={enteredName} />
-			<button on:click={getName} disabled={!enteredName.trim()}>Send Message</button>
+			{:else}
+			<h1>Please enter your name below</h1>
+			{/if}
+			<input type="text" name="username" 
+				   placeholder="Enter your name" 
+				   autocomplete="off"
+				   bind:value={enteredName} />
+			<button on:click={getName} disabled={!enteredName.trim()}>Submit</button>
 		</Route>
 
 		<Route path="/about" component={About} />
